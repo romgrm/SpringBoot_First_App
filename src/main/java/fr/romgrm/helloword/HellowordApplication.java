3259f5221a5c8b94305ee27937eddae3f37a6b21
romgrm <romaingreaume@gmail.com>
2020-11-18 11:37:25 +0100

    helloword_test

---
+++ src/main/java/fr/romgrm/helloword/HellowordApplication.java
@@ -0,0 +1,15 @@
+package fr.romgrm.helloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HellowordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowordApplication.class, args);
+	}
+
+}
+
